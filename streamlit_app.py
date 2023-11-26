462fb398613fff869c35f204b2c63fa3b9dd7a28
TanveeGit <152068241+TanveeGit@users.noreply.github.com>
2023-11-26 13:38:42 +0000

    Update streamlit_app.py
    fixed syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My parent new Healthy  Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal)
+streamlit.text('Omega 3 & Bluberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free range Egg')
 
